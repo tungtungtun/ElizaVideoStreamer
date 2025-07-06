3e65c6276d3c5b38c187815a99b6cbca9985e9bf
tungtungtun <enginemaker31@gmail.com>
2025-07-06 14:35:08 +0530

    Create Eliza.py

---
+++ Driver/Eliza.py
@@ -0,0 +1,9 @@
+# driver/eliza.py
+
+class Eliza:
+    def __init__(self):
+        pass
+
+    def reply(self, text):
+        # Provide a simple placeholder
+        return "Eliza: I don't understand."
